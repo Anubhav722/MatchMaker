69ccca597401bec1f36409c5a24f2377e46c64d3
Anubhav722 <anubhav@launchyard.com>
2017-10-30 01:03:30 +0530

    add new app and template

---
+++ src/questions/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+class AnswerTabularInline(admin.TabularInline):
+	model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+	inlines = [AnswerTabularInline]
+	class Meta:
+		model = Question
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
