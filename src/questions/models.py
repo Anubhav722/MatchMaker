14d9f6beed389adfbeae9aa803a7440b79b8ebc2
Anubhav722 <anubhav@launchyard.com>
2017-11-02 02:16:32 +0530

    add post_save signal and update it with pre_save

--- src/questions/models.py
+++ src/questions/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Question(models.Model):
@@ -35,9 +35,36 @@ class UserAnswer(models.Model):
 	question = models.ForeignKey(Question)
 	my_answer =  models.ForeignKey(Answer, related_name='user_answer')
 	my_answer_importance = models.CharField(max_length=50, choices=LEVELS)
+	my_points = models.IntegerField(default=-1)
 	their_answer = models.ForeignKey(Answer, null=True, blank=True, related_name='match_answer')
 	their_answer_importance = models.CharField(max_length=50, choices=LEVELS)
+	their_points = models.IntegerField(default=-1)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
 	def __str__(self):
 		return self.my_answer.text[:10]
+
+def score_importance(importance_level):
+	if importance_level == "Mandatory":
+		points = 300
+	elif importance_level == "Very Important":
+		points = 200
+	elif importance_level == "Somewhat Important":
+		points = 50
+	else:
+		points = 0
+	return points
+
+def update_user_answer_score(sender, instance, created, *args, **kwargs):
+	if created:
+		if instance.my_points == -1:
+			my_points = score_importance(instance.my_answer_importance)
+			instance.my_points = my_points
+			instance.save()
+
+		if instance.their_points == -1:
+			their_points = score_importance(instance.my_answer_importance)
+			instance.their_points = their_points
+			instance.save()
+
+post_save.connect(update_user_answer_score, sender=UserAnswer)
