b743b2d5760216b8eaec63888db87d3cc71b6762
Anubhav722 <anubhav@launchyard.com>
2017-11-01 23:48:51 +0530

    save user instance in views

--- src/questions/views.py
+++ src/questions/views.py
@@ -5,47 +5,66 @@ from .forms import UserResponseForm
 
 # Create your views here.
 def single(request, id):
-	if request.user.is_authenticated():
-		form = UserResponseForm(request.POST or None)
-		if form.is_valid():
-			print(request.POST)
-			print (form.cleaned_data)
-			question_id = form.cleaned_data.get('question_id')
-			answer_id = form.cleaned_data.get('answer_id')
-			question_instance = Question.objects.get(id=id)
-			answer_instance = Question.objects.get(id=id)
-			print (question_instance.text, answer_instance.text)
-			next_q = Question.objects.all().order_by('?').first()
-			return redirect("question_single", id=next_q.id)
+    if request.user.is_authenticated():
+        form = UserResponseForm(request.POST or None)
+        if form.is_valid():
 
-		queryset = Question.objects.all().order_by('-timestamp')
-		# instance = queryset[0]
-		instance = get_object_or_404(Question, id=id)	
-		context = {
-			"form": form,
-			"instance": instance,
-			# "queryset": queryset
-		}
-		return render(request, "questions/single.html", context)
-	else:
-		raise Http404
+            # fetching all the form data coming via POST.
+            question_id = form.cleaned_data.get('question_id')
+            answer_id = form.cleaned_data.get('answer_id')
+            their_answer_id = form.cleaned_data.get('their_importance_level')
+            importance_level = form.cleaned_data.get('importance_level')
+            their_importance_level = form.cleaned_data.get('their_importance_level')
+            
+            # DB Queries to get querysets.
+            question_instance = Question.objects.get(id=question_id)
+            answer_instance = Question.objects.get(id=answer_id)
+
+            new_user_answer = UserAnswer()
+            new_user_answer.user = request.user
+            new_user_answer.question = question_instance
+            new_user_answer.my_answer = answer_instance
+            new_user_answer.my_answer_importance = importance_level
+
+            if their_answer_id != -1:
+                their_answer_instance = Question.objects.get(id=their_answer_id)
+                new_user_answer.their_answer = their_answer_instance
+                new_user_answer.their_answer_importance = their_answer_importance
+            else:
+                new_user_answer.their_answer_importance = "Not Important"
+            new_user_answer.save()
+
+            next_q = Question.objects.all().order_by('?').first()
+            return redirect("question_single", id=next_q.id)
+
+        queryset = Question.objects.all().order_by('-timestamp')
+        # instance = queryset[0]
+        instance = get_object_or_404(Question, id=id)   
+        context = {
+            "form": form,
+            "instance": instance,
+            # "queryset": queryset
+        }
+        return render(request, "questions/single.html", context)
+    else:
+        raise Http404
 
 def home(request):
 
-	if request.user.is_authenticated():
-		form = UserResponseForm(request.POST or None)
-		if form.is_valid():
-			print (form.cleaned_data)
-			question_id = form.cleaned_data.get('question_id')
-			answer_id = form.cleaned_data.get('answer_id')
+    if request.user.is_authenticated():
+        form = UserResponseForm(request.POST or None)
+        if form.is_valid():
+            print (form.cleaned_data)
+            question_id = form.cleaned_data.get('question_id')
+            answer_id = form.cleaned_data.get('answer_id')
 
-		queryset = Question.objects.all().order_by('-timestamp')
-		instance = queryset[0]
-		context = {
-			"form": form,
-			"instance": instance,
-			# "queryset": queryset
-		}
-		return render(request, "questions/home.html", context)
-	else:
-		raise Http404
+        queryset = Question.objects.all().order_by('-timestamp')
+        instance = queryset[0]
+        context = {
+            "form": form,
+            "instance": instance,
+            # "queryset": queryset
+        }
+        return render(request, "questions/home.html", context)
+    else:
+        raise Http404
